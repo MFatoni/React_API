474ada06db2150b0e5b08a6829bdb9cba6abad8a
Fatoni <mohfatoni@yahoo.com>
2019-11-22 08:50:54 +0700

    Get Data Using Axios & Fetch from API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataApi: []
+    };
+  }
+  componentDidMount() {
+    // fetch("https://jsonplaceholder.typicode.com/posts")
+    //   .then(response => response.json())
+    //   .then(json => {
+    //     this.setState({dataApi: json});
+    //   });
+
+    axios.get("https://jsonplaceholder.typicode.com/posts").then(res => {
+      this.setState({
+        dataApi: res.data
+      });
+    });
+  }
+  render() {
+    return (
+      <div>
+        <p>Hello Api</p>
+        {this.state.dataApi.map((data, index) => {
+          return (
+            <div key={index}>
+              <p>{data.title}</p>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
